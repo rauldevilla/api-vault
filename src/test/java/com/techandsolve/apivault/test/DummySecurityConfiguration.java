1b8fcd571c4cb10e69047fb1562c4d3ddbd13b7f
rauldevilla <raul.devilla@gmail.com>
2020-06-05 10:54:21 -0500

    Fix tests

--- src/test/java/com/techandsolve/apivault/test/DummySecurityConfiguration.java
+++ src/test/java/com/techandsolve/apivault/test/DummySecurityConfiguration.java
@@ -5,22 +5,20 @@ import com.techandsolve.apivault.annotations.CredentialsValidator;
 import com.techandsolve.apivault.annotations.SecurityConfiguration;
 import com.techandsolve.apivault.web.filter.*;
 
-@SecurityConfiguration(credentialsBuilders = {SecurityBearerCredentialsBuilder.class})
+@SecurityConfiguration(credentialsBuilders = {SecurityBearerCredentialsBuilder.class, SecurityCookieTokenCredentialsBuilder.class})
 public class DummySecurityConfiguration {
 
     @AccessValidator
     public boolean hasAccess(Resource resource, Credentials[] credentials) {
-        //HERE, YOUR RESOURCE ACCESS VALIDATION CODE
-        //THIS CODE IS JUST AN EXAMPLE
         return resource.getUri().startsWith("/public/");
     }
 
     @CredentialsValidator
     public boolean isValidCredentials(Credentials credentials) {
-        //HERE, YOUR CREDENTIALS VALIDATION CODE
-        //THIS CODE IS JUST AN EXAMPLE
         if (credentials instanceof BearerTokenCredentials) {
             return ((BearerTokenCredentials)credentials).getToken().startsWith("VALID-");
+        } else if (credentials instanceof CookieTokenCredentials) {
+            return ((CookieTokenCredentials)credentials).getToken().startsWith("VALID-");
         }
 
         return false;
